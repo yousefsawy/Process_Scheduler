4b38446fbc444108806bbf12b2b05f595bdbd12a
A-Aref <abdallahahmadaref@gmail.com>
2023-03-24 07:14:03 +0200

    priqueue

--- PriQueue.h
+++ PriQueue.h
@@ -34,10 +34,9 @@ PriQueue<T>::PriQueue()
 
 
 template<typename T>
-bool PriQueue<T>::isEmpty() const {
-
-	return (front == nullptr && back == nullptr);
-
+bool PriQueue<T>::isEmpty() const 
+{
+	return (front == nullptr);
 }
 
 
